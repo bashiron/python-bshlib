3ab55e089dd8f45042979b6dcd1ba87df5e7277c
Bashiron <bioshiron@gmail.com>
2023-06-12 23:43:42 -0300

    `task_op` decorator and switch-mode operation
    manager now works with its `task` attribute instead of asking for it in every method that works with one. every task-depending method is now also decorated to make sure the `task` attribute is set before working

--- lib/bshlib/output_mgr.py
+++ lib/bshlib/output_mgr.py
@@ -5,9 +5,19 @@ from os import rename, PathLike
 
 from typing import TextIO
 
+def task_op(function):
+    """Check that `task` attribute is set. Can only be used to decorate `OutputMgr`'s methods since it takes an
+    `OutputMgr` object as first argument.
+    """
+    def wrapper(mgr, *args, **kwargs):
+        mgr.task_check()
+        return function(mgr, *args, **kwargs)
+
+    return wrapper
 
 class OutputMgr:
     root: Path
+    task: str | None
 
     def __init__(self, out_root):
         """
@@ -23,6 +33,12 @@ class OutputMgr:
                 self.root = Path(out_root)
             case _:
                 raise TypeError
+        self.task = None
+
+    def switch(self, task):
+        """Switch to operating on `task`.
+        """
+        self.task = task
 
     def create_root(self):
         try:
@@ -30,35 +46,31 @@ class OutputMgr:
         except FileExistsError:
             print('directory exists. skipping..')
 
-    def create_task_root(self, task):
+    @task_op
+    def create_task_root(self):
         try:
-            mkdir(self.root / task)
+            mkdir(self.root / self.task)
         except FileExistsError:
             print('directory exists. skipping..')
 
-    def mkfile(self, task):
+    @task_op
+    def mkfile(self):
         """Opens file in its appropiate task folder. The file is appended with a number.
-
-        Parameters
-        -----
-        task : str
-            Task.
 
         Returns
         -----
         ret : `TextIO`
             Open file handle.
         """
-        return self.__strong_open(self.root / task / f"{task}_{self.next_num(task)}", 'wt')
+        return self.__strong_open(self.root / self.task / f"{self.task}_{self.next_num()}", 'wt')
 
-    def create(self, task, content):
+    @task_op
+    def create(self, content):
         """Creates file in its appropiate task folder. The file is appended with a number. Fills the file with
         provided content.
 
         Parameters
         -----
-        task : `str`
-            Task.
         content : `Any`
             Content to write to file.
 
@@ -67,60 +79,54 @@ class OutputMgr:
         ret : `Path`
             Path of the created file.
         """
-        path = self.root / task / f"{task}_{self.next_num(task)}"
+        path = self.root / self.task / f"{self.task}_{self.next_num()}"
         self.__strong_open(path, 'wt').write(content)
         return path
 
-    def clear(self, task):
+    @task_op
+    def clear(self):
         """Clears task directory.
         """
-        for ch in (self.root / task).iterdir():
+        for ch in (self.root / self.task).iterdir():
             ch.unlink()
 
-    def conventional(self, task):
+    @task_op
+    def conventional(self):
         """Return a list of paths of every file that follows the numeric sequence filename convention.
-
-        Parameters
-        -----
-        task : `str`
-            Task.
 
         Returns
         -----
         ret : `list` of `Path`
             Paths of conventional files.
         """
-        taskdir = self.root / task
+        taskdir = self.root / self.task
         good_files = list(taskdir.iterdir())
-        rx = compile(task + r'_\d+')
+        rx = compile(self.task + r'_\d+')
         pred = lambda e: rx.match(e.stem) is not None
         good_files = list(filter(pred, good_files))
         return good_files
 
-    def rearrange(self, task):
+    @task_op
+    def rearrange(self):
         """Rename the task files so they follow a sequential order again. Useful for gaps in the numeric naming sequence
         caused by file deletion.
-
-        Parameters
-        -----
-        task : `str`
-            Task.
         """
-        files = self.conventional(task)
+        files = self.conventional()
         nums = map(lambda e: self.extract_number(e), files)
         ordered = list(zip(files, nums))
         ordered.sort(key=lambda e: e[1])
 
         i = 0
         for path, _ in ordered:
-            rename(path, path.with_stem(f"{task}_{i}"))
+            rename(path, path.with_stem(f"{self.task}_{i}"))
             i += 1
 
-    def next_num(self, task):
+    @task_op
+    def next_num(self):
         """Get next file number for task file.
         """
-        taskdir = self.root / task
-        files = self.conventional(task)
+        taskdir = self.root / self.task
+        files = self.conventional()
 
         if not taskdir.exists() or len(files) == 0:
             num = 0
@@ -139,6 +145,8 @@ class OutputMgr:
         rx = compile(r'.*_(\d+)')
         return int(rx.match(path.stem).group(1))
 
+# ----- MISC -----
+
     def __strong_open(self, path, mode):
         """Open file, creating parent directory if doesn't exist.
 
@@ -154,3 +162,7 @@ class OutputMgr:
             # retry
             fd = self.__strong_open(path, mode)
         return fd
+
+    def task_check(self):
+        if self.task is None:
+            raise UnboundLocalError('task not yet set')
